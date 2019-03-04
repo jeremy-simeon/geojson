552bc0e24febe679a5cfb2ca80b32d15b13250f6
Jérémy SIMEON <jeremy.simeon@gmail.com>
2019-03-04 10:48:41 +0100

    CSS

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-vh-100 d-flex flex-column">
     <header>
-      <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar toggleable="lg" type="dark" variant="dark" fixed>
         <b-navbar-brand :to="{ name: 'index' }">GEOJSON</b-navbar-brand>
         <b-navbar-toggle target="nav_collapse"/>
         <b-collapse is-nav id="nav_collapse">
